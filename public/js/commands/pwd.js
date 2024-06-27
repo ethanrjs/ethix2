ef0bf0c5d30bd05daf51ef14e6bf58af819c9527
babbur <ethanrrelectric@gmail.com>
2024-06-27 14:01:06 -0400

    remove unnecessary import from pwd

--- public/js/commands/pwd.js
+++ public/js/commands/pwd.js
@@ -3,7 +3,6 @@ import {
     getCurrentDirectory,
     registerCommand
 } from '../terminal.js';
-import { registerCommandDescription } from './help.js';
 
 registerCommand('pwd', 'Print the working directory', args => {
     addOutputLine(getCurrentDirectory());
